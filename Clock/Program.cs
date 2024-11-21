d5b6217d288d43fd41f64110f51b157c0a1384a2
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-21 10:41:41 +0100

    Commit

---
+++ Clock/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    // Metoda koja pretvara broj dana u sate i sekunde
+    static void ConvertDaysToHoursAndSeconds(int days, out int hours, out int seconds)
+    {
+        // 1 dan = 24 sata, 1 sat = 3600 sekundi
+        hours = days * 24;
+        seconds = days * 24 * 3600;
+    }
+
+    static void Main()
+    {
+        // Primer upotrebe
+        int days = 2; // Unesite broj dana
+        int hours, seconds;
+
+        // Pozivanje metode
+        ConvertDaysToHoursAndSeconds(days, out hours, out seconds);
+
+        // Ispis rezultata
+        Console.WriteLine($"{days} dana = {hours} sati i {seconds} sekundi.");
+    }
+}
